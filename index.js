5179904bc332704695d4816c01608fb9edb9cabd
vii <vii@divero.net>
2020-02-01 17:24:53 +0100

    RSS

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ var permalinks = require('metalsmith-permalinks');
 var collections = require('metalsmith-collections');
 var handlebars = require('handlebars');
 var assets = require('metalsmith-assets');
+const feed = require('metalsmith-feed');
 
 handlebars.registerHelper('dateFormat', require('handlebars-dateformat'));
 
 Metalsmith(__dirname)
   .metadata({
+    site: {
+      title: 'vii @ DIVcompo 2020',
+      url: 'https://divcompo2020.now.sh/',
+      author: 'vii'
+    },
     sitename: 'vii @ DIVcompo 2020',
     siteurl: 'https://divcompo2020.now.sh/',
     author: 'vii',
@@ -36,19 +42,20 @@ Metalsmith(__dirname)
     source: 'static',
     destination: 'static'
   }))
-  .use(
-    layouts({
-      engine: 'handlebars',
-      directory: 'layouts',
-      //default: 'article.hbs',
-      partialExtension: 'hbs',
-      //pattern: ["**/*.hbs"],
-      partials: {
-        header: 'partials/header',
-        footer: 'partials/footer'
-      }
-    })
-  )
+  .use(layouts({
+    engine: 'handlebars',
+    directory: 'layouts',
+    //default: 'article.hbs',
+    partialExtension: 'hbs',
+    //pattern: ["**/*.hbs"],
+    partials: {
+      header: 'partials/header',
+      footer: 'partials/footer'
+    }
+  }))
+  .use(feed({
+    collection: 'articles'
+  }))
   .build(function(err, files) {
     if (err) {
       throw err;
